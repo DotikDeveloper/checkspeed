b4ddc37d11446770d0559e515ccb3bb61d2302ca
Sergei Popov <spopov@dotdev.site>
2024-10-26 11:50:48 +0400

    clear init app

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,55 +1,74 @@
 "use client";
 
-import { useState, useEffect } from "react";
-import { measureInternetSpeed } from '@/utils/checkspeed';
 import { ISpeed } from "@/utils/types";
+import { useEffect, useState } from "react";
 
 export default function Home() {
   const [speed, setSpeed] = useState<ISpeed | null>(null);
 
-  const handleMeasureSpeed = async () => {
-    setSpeed(null);
-    const measuredSpeed = await measureInternetSpeed();
-    // setSpeed(measuredSpeed);
+  setTimeout(() => {
+      setSpeed({ speedUpload: 100, speedDownload: 90, ping: 3 });
+  }, 3000);
 
-    setTimeout(() => {
-      setSpeed({  ping: 3, speedDownload: 100, speedUpload: 115 });
-    }, 3000);
-
-    console.log("measuredSpeed --->", measuredSpeed);
-    
-  };
-
-  useEffect(() => {
-    handleMeasureSpeed();
-  }, []);
+  const handleMeasureSpeed = (): void => {
+    console.log("click btn");
+    // перезагрузить страницу
+      window.location.reload();
+  }
 
   return (
     <div className="flex flex-col items-center justify-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
       <main className="flex flex-col items-center gap-8">
         <div className="grid grid-cols-1 sm:grid-cols-3 gap-8">
-          <div className="bg-gray-800 rounded-lg p-6 text-center">
-            <h2 className="text-2xl font-semibold text-gray-400 mb-2">Отправка</h2>
-            <p className="text-4xl font-bold text-white">
-              {speed !== null ? `${speed.speedUpload.toFixed(2)} МБ/с` : 'Измерение...'}
-            </p>
-          </div>
-          <div className="bg-gray-800 rounded-lg p-6 text-center">
-            <h2 className="text-2xl font-semibold text-gray-400 mb-2">Получение</h2>
-            <p className="text-4xl font-bold text-white">
-              {speed !== null ? `${speed.speedDownload.toFixed(2)} МБ/с` : 'Измерение...'}
-            </p>
-          </div>
-          <div className="bg-gray-800 rounded-lg p-6 text-center">
-            <h2 className="text-2xl font-semibold text-gray-400 mb-2">Пинг</h2>
-            <p className="text-4xl font-bold text-white">
-              {speed !== null ? `${speed.ping.toFixed(0)} мс` : 'Измерение...'}
-            </p>
-          </div>
+          {!speed ? (
+            <div className="bg-gray-800 rounded-lg p-6 text-center">
+              <h2 className="text-2xl font-semibold text-gray-400 mb-2">Отправка</h2>
+              <div className="h-10 w-32 text-4xl font-bold text-white bg-gray-600 rounded mx-auto animate-pulse">000</div>
+              <span className="text-2xl font-bold text-white">Мбит/с</span>
+            </div>
+          ) : (
+            <div className="bg-gray-800 rounded-lg p-6 text-center">
+              <h2 className="text-2xl font-semibold text-gray-400 mb-2">Отправка</h2>
+              <p className="text-4xl font-bold text-white">
+                {speed.speedUpload.toFixed(2)}
+              </p>
+              <span className="text-2xl font-bold text-white">Мбит/с</span>
+            </div>
+          )}
+          {!speed ? (
+            <div className="bg-gray-800 rounded-lg p-6 text-center">
+              <h2 className="text-2xl font-semibold text-gray-400 mb-2">Получение</h2>
+              <div className="h-10 w-32 text-4xl font-bold text-white bg-gray-600 rounded mx-auto animate-pulse">000</div>
+              <span className="text-2xl font-bold text-white">Мбит/с</span>
+            </div>
+          ) : (
+            <div className="bg-gray-800 rounded-lg p-6 text-center">
+              <h2 className="text-2xl font-semibold text-gray-400 mb-2">Получение</h2>
+              <p className="text-4xl font-bold text-white">
+                {speed.speedDownload.toFixed(2)}
+              </p>
+              <span className="text-2xl font-bold text-white">Мбит/с</span>
+            </div>
+          )}
+          {!speed ? (
+            <div className="bg-gray-800 rounded-lg p-6 text-center">
+              <h2 className="text-2xl font-semibold text-gray-400 mb-2">Пинг</h2>
+              <div className="h-10 w-32 text-4xl font-bold text-white bg-gray-600 rounded mx-auto animate-pulse">000</div>
+              <span className="text-2xl font-bold text-white">мс</span>
+            </div>
+          ) : (
+            <div className="bg-gray-800 rounded-lg p-6 text-center">
+              <h2 className="text-2xl font-semibold text-gray-400 mb-2">Пинг</h2>
+              <p className="text-4xl font-bold text-white">
+                {speed.ping.toFixed(0)}
+              </p>
+              <span className="text-2xl font-bold text-white">мс</span>
+            </div>
+          )}
         </div>
-        <button 
+        <button
           className="rounded-md px-12 py-3 text-lg font-medium text-white bg-blue-500 hover:bg-blue-600 transition duration-200"
-          onClick={handleMeasureSpeed}
+          onClick={(handleMeasureSpeed)}
         >
           Измерить
         </button>
